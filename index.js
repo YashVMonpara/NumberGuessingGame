0e7978786099b3be92b5373a788966512f6293f9
Yash Monpara <64018426+YashVMonpara@users.noreply.github.com>
2021-01-12 20:47:30 +0530

    Update index.js
    Remove unnecessary code

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
-    console.log(guess, typeof guess);
 
     if (!guess) {
         document.querySelector('.message').textContent = 'No Number...';
